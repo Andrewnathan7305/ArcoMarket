50300af9d74a1b3ae2eb1f0078726c8799d9e508
Andrewnathan <94670957+Andrewnathan7305@users.noreply.github.com>
2025-10-15 19:39:18 +0530

    Added arcology boilerplate code

---
+++ blockend/hardhat.config.js
@@ -0,0 +1,35 @@
+require("@nomiclabs/hardhat-waffle");
+require('dotenv').config()
+const nets = require('./network.json');
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+        settings: { 
+          optimizer: { 
+            enabled: true, 
+            runs: 1000 
+          },
+          viaIR: true
+        },
+      },
+      {
+        version: "0.4.18",
+        settings: { optimizer: { enabled: true, runs: 200 } },
+        viaIR: true,
+      }
+    ],
+    overrides: {
+      "contracts/WETH9.sol": {
+        version: "0.4.18",
+        settings: { optimizer: { enabled: true, runs: 200 } },
+      }
+    }
+  },
+  networks: nets
+};
