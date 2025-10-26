09cf63141e4f04cc44059e6e76b1f473ce1714e9
Andrew <andrewnathan0704@gmail.com>
2025-10-26 18:37:17 +0530

    Add new Market.sol file with a series of yes/no questions related to current events and policies.

---
+++ polygon_anvil_test/Market.sol
@@ -0,0 +1,20 @@
+Will the new US citizenship test start after October 20, 2025? (Yes/No)
+
+Has the US government updated immigration petition rules recently? (Yes/No)
+
+Is the UK Parliament debating new policies this week? (Yes/No)
+
+Did India launch a rural women credit scheme in Bihar? (Yes/No)
+
+Is Solar Orbiter a NASA and ESA joint mission? (Yes/No)
+
+Does Delhi’s pollution significantly reduce life expectancy? (Yes/No)
+
+Was a new missile system recently developed by China? (Yes/No)
+
+Will early citizenship applicants take the older civics test? (Yes/No)
+
+Is the Assisted Dying Bill under review in UK’s House of Lords? (Yes/No)
+
+Has USCIS introduced stricter moral character checks? (Yes/No)
+
